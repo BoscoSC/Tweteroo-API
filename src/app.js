ceac52d52220e01e540de1490c8300fc66bcf963
Joao Bosco <jboscos.carvalho@gmail.com>
2023-01-07 19:32:08 -0300

    feat: route post /sign-up

--- src/app.js
+++ src/app.js
@@ -7,4 +7,21 @@ app.use(express.json());
 
 const PORT = 5000;
 
+const users = [];
+const messages = [];
+
+app.post("/sign-up", (req, res) => {
+  const { username, avatar } = req.body;
+
+  if (!username || !avatar) {
+    return res.status(400).send("Informações faltando");
+  }
+
+  res.status(201).send("OK");
+});
+
+app.post("/tweets", (req, res) => {});
+
+app.get("/tweets", (req, res) => {});
+
 app.listen(PORT, () => console.log(`Running in port: ${PORT}`));
